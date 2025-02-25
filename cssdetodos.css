2a0a8eca385fe00b800addab6b5b447e350c092c
viyanija <viyanija@gmail.com>
2025-02-25 07:41:49 -0300

    Create cssdetodos.css

---
+++ cssdetodos.css
@@ -0,0 +1,90 @@
+.canto{
+	position: absolute;
+	top: 0;
+	left: 0;
+	z-index: -1;
+	opacity: 1;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #000000;
+border: 3px solid;
+  border-radius: 15px;
+text-align: center;
+
+ display: flex;
+ flex-direction: row;
+justify-content: center;
+
+}
+
+li {
+  float: left;
+border-right: 1px solid #bbbbbb;
+background-color: #444444
+
+}
+
+li a {
+	display: inline-block;
+	color: white;
+	text-align: center;
+	padding: 14px 16px;
+	text-decoration: none;
+}
+
+li a:hover {
+  background-color: #222222;
+}
+
+body {
+	background-color: #cccccc;
+	font-family: sans-serif;
+}
+
+h1 {
+	text-align: center;
+}
+
+h2 {
+	text-align: center;
+}
+
+h4 {
+	text-align: center;
+}
+
+p{
+	padding: 10px 15%;
+	line-height: 1.3;
+}
+
+figcaption{
+	font-size: 14px;
+	font-style: italic;
+	font-family: serif;
+	letter-spacing: 1px;
+}
+
+
+
+
+@media screen and (max-width: 600px) {
+  
+  ul li {
+float: none;
+}
+ ul {
+  flex-direction: column; 
+}
+p { 
+padding: 10px 10px 10px 50px;
+}
+h1 {
+	padding: 50px 0px 0px 0px;
+}
+}
